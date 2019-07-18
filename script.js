5595daf3ba7c8b81c102f2c30da1436fd7150570
heitor <heitorbc3@gmail.com>
2019-07-18 17:56:57 -0300

    Adicioanr javascript

---
+++ script.js
@@ -0,0 +1,46 @@
+let nome = (prompt('Digite seu nome'));{
+    confirm("Seu nome é " + nome + ".");
+    
+}
+let idade = Number(prompt('Digite sua idade'));{
+    confirm("Voce tem " + idade + " anos.");
+}
+
+let matri = Number(prompt('Digite sua matricula'));{
+confirm("Voce esta no " + matri + " ano" );
+ 
+}
+let ano = (prompt(" Digite o ano"));
+
+if (ano==2019){
+    console.log("Acertou");
+}
+
+else if (ano==2019){
+    console.log("Acertou");
+}
+
+else{
+    console.log("Errou");
+}
+
+let senha = (prompt("Digite a senha"));
+
+if (senha==123455){
+    console.log("Acertou a senha")
+}
+
+else if (senha==123455){
+    console.log("Acertou a senha")
+}
+else{
+    console.log("Errou a senha")
+}
+
+
+let nota1 = 8;
+let nota2 = 6;
+
+console.log("Eu me chamo " + nome + ", tenho " + idade + " anos e estou estudando JavaScript");
+console.log("O aluno " + nome + ",matricula " + matri + " ,obteve a media final " + ((nota1 + nota2) / 2) + ".");
+
